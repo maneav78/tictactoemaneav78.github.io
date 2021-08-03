2fdb80dbbd8efb87cf1f5a72399f9300178c6427
maneav78 <avoyan.mane2016@gmail.com>
2021-08-03 22:42:53 +0400

    1  bug fixed

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ for (let i = 1; i <= 9; i++) {
 blocks.forEach(function (block) {
   block.addEventListener("click", function () {
     if (count == 1 && block.innerHTML == "") {
-      block.innerHTML = '<p>X</p>';
+      block.innerHTML = "<p>X</p>";
       count = 2;
       finish();
     } else if (count == 2 && block.innerHTML == "") {
-      block.innerHTML = '<p>O</p>';
+      block.innerHTML = "<p>O</p>";
       count = 1;
       finish();
     } else {
@@ -39,19 +39,31 @@ function finish() {
 }
 
 function whoWinner() {
-  if ( blocks[1].innerHTML == blocks[2].innerHTML && blocks[1].innerHTML == blocks[3].innerHTML){
+  if (
+    blocks[1].innerHTML == blocks[2].innerHTML &&
+    blocks[1].innerHTML == blocks[3].innerHTML
+  ) {
     alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
-  }else if (blocks[4].innerHTML == blocks[5].innerHTML && blocks[4].innerHTML == blocks[6].innerHTML){
-    alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
-  }else if ( blocks[7].innerHTML == blocks[8].innerHTML && blocks[7].innerHTML == blocks[9].innerHTML){
-    alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
-  }else if ( blocks[1].innerHTML == blocks[5].innerHTML && blocks[1].innerHTML == blocks[9].innerHTML){
+  } else if (
+    blocks[4].innerHTML == blocks[5].innerHTML &&
+    blocks[4].innerHTML == blocks[6].innerHTML
+  ) {
+    alert(`Winner is ${blocks[4].innerHTML.charAt(3)}`);
+  } else if (
+    blocks[7].innerHTML == blocks[8].innerHTML &&
+    blocks[7].innerHTML == blocks[9].innerHTML
+  ) {
+    alert(`Winner is ${blocks[7].innerHTML.charAt(3)}`);
+  } else if (
+    blocks[1].innerHTML == blocks[5].innerHTML &&
+    blocks[1].innerHTML == blocks[9].innerHTML
+  ) {
     alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
   } else if (
     blocks[3].innerHTML == blocks[5].innerHTML &&
     blocks[3].innerHTML == blocks[7].innerHTML
   ) {
-    alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
+    alert(`Winner is ${blocks[3].innerHTML.charAt(3)}`);
   } else if (
     blocks[1].innerHTML == blocks[4].innerHTML &&
     blocks[1].innerHTML == blocks[7].innerHTML
@@ -61,13 +73,13 @@ function whoWinner() {
     blocks[2].innerHTML == blocks[5].innerHTML &&
     blocks[2].innerHTML == blocks[8].innerHTML
   ) {
-    alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
+    alert(`Winner is ${blocks[2].innerHTML.charAt(3)}`);
   } else if (
     blocks[3].innerHTML == blocks[6].innerHTML &&
     blocks[3].innerHTML == blocks[9].innerHTML
   ) {
-    alert(`Winner is ${blocks[1].innerHTML.charAt(3)}`);
-  }else{
-    alert('Nobody won');
+    alert(`Winner is ${blocks[3].innerHTML.charAt(3)}`);
+  } else {
+    alert("Nobody won");
   }
 }
